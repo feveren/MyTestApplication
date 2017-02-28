542b753e882e371d50f8f82bd933e607a632ef36
RenT <feveren@163.com>
2017-02-28 23:36:03 +0800

    day07

--- app/src/test/java/com/rent/mytestapplication/homework/Daily.java
+++ app/src/test/java/com/rent/mytestapplication/homework/Daily.java
@@ -87,4 +87,25 @@ public class Daily {
         }
         return arr;
     }
+
+    @Test
+    public void lowerAndUpper() {
+        String str = "OkhaoPingCeilXu";
+        // 复杂度O(n)
+        char[] arr = str.toCharArray();
+        char[] newArr = new char[arr.length];
+        int index = arr.length - 1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] >= 'A' && arr[i] <= 'Z') {
+                newArr[index--] = arr[i];
+            }
+        }
+        index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 'a' && arr[i] <= 'z') {
+                newArr[index++] = arr[i];
+            }
+        }
+        System.out.println(newArr);
+    }
 }
