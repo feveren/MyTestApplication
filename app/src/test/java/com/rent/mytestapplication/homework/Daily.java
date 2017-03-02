d0005397e8e9fe11ff8c80260560c842960776af
RenT <feveren@163.com>
2017-03-02 15:59:41 +0800

    之前的每日一题都放到了该类中

--- app/src/test/java/com/rent/mytestapplication/homework/Daily.java
+++ app/src/test/java/com/rent/mytestapplication/homework/Daily.java
@@ -5,10 +5,63 @@ import org.junit.Test;
 import java.util.Arrays;
 
 /**
+ *
  * Created by RenTao on 17/2/26.
  */
-
 public class Daily {
+
+    /**
+     * 二分法
+     */
+    @Test
+    public void splitFind() {
+        int[] arr = randomArray(10);
+        Arrays.sort(arr);
+        System.out.println("origin: " + Arrays.toString(arr));
+        int target = (int) (Math.random() * arr.length);
+        System.out.println("target: " + target);
+        int start = 0, end = arr.length;
+        int mid = arr.length / 2;
+        int index = -1;
+        while (end > start) {
+            System.out.println("arr[mid]: " + arr[mid] + ", start: " + start + ", mid: " + mid + ", end: " + end);
+            if (target < arr[mid]) {
+                end = mid - 1;
+                mid = (start + end) / 2;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+                mid = (start + end) / 2;
+            } else {
+                index = mid;
+                break;
+            }
+            System.out.println("start: " + start + ", mid: " + mid + ", end: " + end);
+        }
+        System.out.println("index: " + index);
+    }
+
+    /**
+     * 冒泡排序
+     */
+    @Test
+    public void testBubbling() {
+        int[] arr = randomArray(10);
+        bubbling(arr, true);
+        bubbling(arr, false);
+    }
+
+    private void bubbling(int[] arr, boolean asc) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if ((asc && arr[j] > arr[j + 1]) // 升序
+                        || (!asc && arr[j] < arr[j + 1])) { // 降序
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+        System.out.print(asc ? "asc: " : "desc: ");
+        System.out.println(Arrays.toString(arr));
+    }
 
     @Test
     public void findLongestString() {
@@ -40,13 +93,12 @@ public class Daily {
     }
 
     private void quickSort(int[] arr, int start, int end) {
+        if (end - start <= 0) {
+            return;
+        }
         int keyIndex = splitArray(arr, start, end);
-        if (keyIndex - start > 1) {
-            quickSort(arr, start, keyIndex - 1);
-        }
-        if (end - keyIndex > 1) {
-            quickSort(arr, keyIndex + 1, end);
-        }
+        quickSort(arr, start, keyIndex - 1);
+        quickSort(arr, keyIndex + 1, end);
     }
 
     private int splitArray(int[] arr, int start, int end) {
@@ -107,5 +159,14 @@ public class Daily {
             }
         }
         System.out.println(newArr);
+    }
+
+    /**
+     * 从扑克牌中随机抽 5 张牌,判断是不是顺子,即这 5 张牌是不是连续的。
+     * 2-10 为数字本身,A 为 1,J 为 11,Q 为 12,K 为 13,而大小王可以看成任意的数字。
+     */
+    @Test
+    public void random5() {
+
     }
 }
