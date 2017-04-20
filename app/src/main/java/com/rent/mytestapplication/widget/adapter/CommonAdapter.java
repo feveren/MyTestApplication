340e3ec2492a1a633f2d5100746d29a347e1c67f
RenT <feveren@163.com>
2017-04-20 23:20:21 +0800

    增加多布局的支持

--- app/src/main/java/com/rent/mytestapplication/widget/adapter/CommonAdapter.java
+++ app/src/main/java/com/rent/mytestapplication/widget/adapter/CommonAdapter.java
@@ -1,6 +1,7 @@
 package com.rent.mytestapplication.widget.adapter;
 
 import android.content.Context;
+import android.util.SparseIntArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,21 +10,32 @@ import android.widget.BaseAdapter;
 import java.util.List;
 
 /**
- * 通用的Adapter，实现{@link #convert(int, ViewHolder, Object)}即可快速的构造一个Adapter
+ * 通用的Adapter，实现{@link #convert(int, ViewHolder, T)}即可快速的构造一个Adapter
  * Created by RenT on 2016/4/23.
  */
 public abstract class CommonAdapter<T> extends BaseAdapter {
     protected Context mContext;
     protected List<T> mData;
     protected LayoutInflater mInflater;
-    protected int mLayoutId;
+    protected SparseIntArray mViewTypes;
     protected ViewGroup mParent;
 
-    public CommonAdapter(Context context, List<T> data, int layoutId) {
+    /**
+     * 假如使用该构造方法，必须使用{@link #addViewType(int, int)}添加布局
+     */
+    public CommonAdapter(Context context, List<T> data) {
         this.mContext = context;
         mInflater = LayoutInflater.from(context);
         this.mData = data;
-        this.mLayoutId = layoutId;
+        this.mViewTypes = new SparseIntArray();
+    }
+
+    /**
+     * 单个布局
+     */
+    public CommonAdapter(Context context, List<T> data, int layoutId) {
+        this(context, data);
+        addViewType(0, layoutId);
     }
 
     @Override
@@ -42,13 +54,34 @@ public abstract class CommonAdapter<T> extends BaseAdapter {
     }
 
     @Override
+    public int getItemViewType(int position) {
+        if (mViewTypes.size() > 1) {
+            throw new IllegalStateException("You must override getItemViewType(int) when added more than 1 viewType");
+        }
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        if (mViewTypes.size() == 0) {
+            throw new IllegalStateException("add 1 viewType at least");
+        }
+        return mViewTypes.size();
+    }
+
+    public void addViewType(int viewType, int layoutId) {
+        mViewTypes.put(viewType, layoutId);
+    }
+
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (mParent == null) {
             this.mParent = parent;
         }
         ViewHolder holder;
         if (convertView == null) {
-            convertView = mInflater.inflate(mLayoutId, parent, false);
+            int layoutId = mViewTypes.get(getItemViewType(position));
+            convertView = mInflater.inflate(layoutId, parent, false);
             holder = new ViewHolder(convertView);
             convertView.setTag(holder);
         } else {
